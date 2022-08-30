e4c0b961c6655d249efdb025a3506be8113faa92
kai_oishi <>
2022-08-30 18:54:51 +0900

    firstCommit

---
+++ UItabberTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UItabberTraining
+//
+//  Created by kai_oishi on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
